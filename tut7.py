0d583ad51d53fd50554b8e478c9930a5c8872a2d
vats147 <89577062+vats147@users.noreply.github.com>
2021-12-10 15:31:13 +0530

    Add files via upload

---
+++ tut7.py
@@ -0,0 +1,30 @@
+#Topic   :- Comments, Escape Sequences & Print Statement
+#lecture := 6
+
+# This is single line comment
+
+"""
+    This
+    is
+    multiline
+    comment
+
+"""
+
+# This is print statement
+print("hello")
+print("world")
+"""
+if we run both print statement we give output as in newline like 
+output: hello
+        world
+        
+but what if we need to need output on same line 
+so here's are the solution
+
+"""
+#end say that print is not eneded yet so world is appended to hello with white space
+print("hello",end=" ")
+print("world")
+
+
---
+++ tut8.py
@@ -0,0 +1,30 @@
+#Topic   :- Variables, Datatypes and Typecasting
+#lecture := 7
+#quiz
+
+
+# In python main advantage is that we not need to specify type of what type of variable it is
+
+# How to define variable?
+num1=5                #integer variable
+var2=10.24            #float variable
+var3=" Hello world! " #string variable
+# yeah! it is as simple as that
+
+print(num1,var2,var3)
+
+#tyepcasting
+#           means change type of variable from string to integer or float to intger and so on..
+var3="34"
+var4="85"
+print(int(var3)+int(var4)) #Here we change the type of variable from string to integer and add it
+
+# How to print any statement 10 times?
+# print(10 * "Hello world\n")
+#==========================
+# quiz -1
+# Take two number as an input and print their sum
+a=input()
+b=input()
+print(int(a)+int(b))
+
